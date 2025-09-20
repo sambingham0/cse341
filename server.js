e3d9dab1ce8e0c7986043c033e07f059bd574c12
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2025-09-20 15:13:42 -0600

    server

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the CSE341 assignment!');
-});
-
-app.get('/name', (req, res) => {
-  res.send('Sam Bingham'); // Replace with the name of someone you know
+  res.send('Benjamin Franklin');
 });
 
 app.listen(PORT, () => {

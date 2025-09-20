c613c6b358b0d69f46c9bfb3835c77f450708b03
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2025-09-20 15:18:31 -0600

    structure

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Benjamin Franklin');
-});
+
+const nameController = require('./controllers/nameController');
+
+app.get('/', nameController.getName);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

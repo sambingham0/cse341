4687e0ea0eebc4035d51b1f3144201bcb737a00e
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2025-10-10 16:27:25 -0600

    https

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/contacts');
 
+/**
+ * @swagger
+ * /contacts:
+ *   get:
+ *     description: Get all contacts or a specific contact by ID using query parameter
+ *     parameters:
+ *       - name: id
+ *         in: query
+ *         type: string
+ *         description: Optional contact ID to retrieve a specific contact
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved contact(s)
+ *       204:
+ *         description: No contacts found
+ */
+
 
 // GET /contacts optional query param id
 router.get('/', (req, res, next) => {
@@ -12,6 +29,39 @@ router.get('/', (req, res, next) => {
 	}
 });
 
+/**
+ * @swagger
+ * /contacts:
+ *   post:
+ *     description: Create a new contact
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         description: Contact information to create
+ *         schema:
+ *           type: object
+ *           properties:
+ *             firstName:
+ *               type: string
+ *               example: "John"
+ *             lastName:
+ *               type: string
+ *               example: "Doe"
+ *             email:
+ *               type: string
+ *               example: "john.doe@email.com"
+ *             favoriteColor:
+ *               type: string
+ *               example: "Blue"
+ *             birthday:
+ *               type: string
+ *               example: "1990-05-15"
+ *     responses:
+ *       201:
+ *         description: Contact successfully created
+ *       400:
+ *         description: Invalid request body or missing required fields
+ */
 // POST /contacts - create a new contact
 router.post('/', controller.createContact);
 
